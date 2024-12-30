cd702dbdbc55955058d8dcca6925a53ada73c2ad
zach <ke3n1@proton.me>
2024-12-29 20:59:26 -0500

    refactored

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,112 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Gui {
-    private JFrame frame;
-    private JTextArea mainTextArea;
-    private JTabbedPane tabPane;
-    private JToolBar toolbar;
 
     public Gui(){
         initComponents();
     }
 
-    public JTextArea initMainTextArea(){
-
-        this.mainTextArea = new JTextArea();
-        Dimension frameDimension = this.getFrameDimension();
-        this.mainTextArea.setPreferredSize(this.setTextAreaDimension(frameDimension));
-
-
-        return this.mainTextArea;
-    }
-
-    public Dimension setTextAreaDimension(Dimension frameDimension){
-
-        int textAreaHeight = frameDimension.height - 50;
-        int textAreaWidth = frameDimension.width - 50;
-
-        return new Dimension(textAreaWidth, textAreaHeight);
-
-    }
-
-    public JTextArea getMainTextArea(){
-
-        if(this.mainTextArea == null){
-            return this.initMainTextArea();
+    private JToolBar initToolbar(){
+        JToolBar toolbar = new JToolBar();
+        List<JButton> buttons = initToolbarButtons();
+        for(JButton button : buttons){
+            toolbar.add(button);
         }
-        return this.mainTextArea;
-    }
-
-    public JTabbedPane initTabPane(){
-
-        this.tabPane = new JTabbedPane();
-        return this.tabPane;
-    }
-
-    public JTabbedPane getTabPane(){
-
-        if(this.tabPane == null){
-            return this.initTabPane();
-        }
-        return this.tabPane;
-    }
-
-    public JToolBar initToolBar(){
-
-        this.toolbar = new JToolBar();
-        return this.toolbar;
+        return toolbar;
 
     }
 
-    public JToolBar getToolBar(){
-
-        if(this.toolbar == null){
-            return this.initToolBar();
-        }
-
-        return this.toolbar;
-    }
-
-
-    public JFrame initFrame(){
-
-        this.frame = new JFrame("cryptopad");
-        this.frame.setPreferredSize(new Dimension(800, 1000));
-        this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        return this.frame;
-    }
-
-    public JFrame getFrame(){
-
-        if(this.frame == null){
-            return this.initFrame();
-        }
-
-        return this.frame;
-    }
-
-    public Dimension getFrameDimension(){
-
-        return this.getFrame().getPreferredSize();
+    private List<JButton> initToolbarButtons(){
+        ArrayList<JButton> buttons = new ArrayList<>();
+        JButton settings = new JButton("settings");
+        buttons.add(settings);
+        return buttons;
 
     }
 
-    public void showFrame(){
-
-        this.frame.setVisible(true);
-
+    private JTabbedPane initTabbedPane(){
+        JTabbedPane tabPane = new JTabbedPane();
+        tabPane.add("untitled", initUntitledInitPanel());
+        return tabPane;
     }
 
-    public void initComponents(){
+    private JPanel initUntitledInitPanel(){
+        JPanel panel = new JPanel(new BorderLayout());
+        JTextArea textArea = new JTextArea();
+        panel.add(textArea, BorderLayout.CENTER);
+        return panel;
+    }
 
+    private JFrame initFrame(){
+        JFrame frame = new JFrame("cryptopad");
+        frame.setPreferredSize(new Dimension(800, 1000));
+        frame.setLayout(new BorderLayout());
+        return frame;
+    }
+
+    private void initComponents(){
         JFrame frame = initFrame();
-        JTextArea mainText = getMainTextArea();
-        frame.add(mainText);
+        JToolBar toolbar = initToolbar();
+        frame.add(toolbar, BorderLayout.NORTH);
+        JTabbedPane tabPane = initTabbedPane();
+        frame.add(tabPane, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
-        showFrame();
-    }
+        frame.setVisible(true);
 
+    }
 
 
 }
