5e1d9bfc8ecf4b580441f341a92feed59b2c1a6d
zach <ke3n1@proton.me>
2025-01-16 22:39:54 -0500

    comments, refactor.

--- src/main/java/gui/Gui.java
+++ src/main/java/gui/Gui.java
@@ -12,24 +12,26 @@ import java.io.File;
 * gui view class.
 */
 public class Gui {
-
+    //basic components of our gi
     private JToolBar toolBar;
     private JButton fileButton;
-    private JButton editButton;
-    private JTabbedPane tabPane;
-    private JFrame frame;
     private JMenuBar menuBar;
     private JMenuItem saveItem;
     private JMenuItem openItem;
     private JMenuItem saveAsItem;
     private JMenuItem newItem;
     private JMenuItem cipherItem;
+    private JButton editButton;
+    private JTabbedPane tabPane;
+    private JFrame frame;
     private GuiController controller;
 
+    //constructor
     public Gui(){
         initComponents();
     }
 
+    //get the controller associated with the gui
     public GuiController getController(){
         if(this.controller == null){
             initController();
@@ -37,10 +39,12 @@ public class Gui {
         return this.controller;
     }
 
+    //init the controller
     public void initController(){
         this.controller = new GuiController(this, new FileModel());
     }
 
+    //get the tabbed pane where all our text files are opened and displayed
     public JTabbedPane getTabbedPane(){
 
         if(this.tabPane == null){
@@ -50,6 +54,14 @@ public class Gui {
 
     }
 
+    //init the tab pane
+    private void initTabbedPane(){
+
+        this.tabPane  = new JTabbedPane();
+        //addNewTab("untitled", "");
+    }
+
+    //get the main gui frame
     public JFrame getFrame(){
 
         if(this.frame == null){
@@ -58,6 +70,7 @@ public class Gui {
         return this.frame;
     }
 
+    //init the main gui frame
     private void initFrame(){
 
         this.frame = new JFrame("cryptopad");
@@ -65,6 +78,7 @@ public class Gui {
         this.frame.setLayout(new BorderLayout());
     }
 
+    //add a new tab given a title and the content for the tab
     public void addNewTab(String title, String content){
 
         //create the panel for the content provided
@@ -100,6 +114,8 @@ public class Gui {
         textArea.getDocument()
                 .addDocumentListener(new UnsavedChangesHandler(textArea, this.getController().getFileModelForTab(title)));
     }
+
+    //get the content of a tab given a title
     public String getTabContent(String title){
         int index = this.getTabbedPane().indexOfTab(title);
         JTextArea textArea = getTextAreaForIndex(index);
@@ -110,6 +126,7 @@ public class Gui {
 
     }
 
+    //get the text area for a tab given an index
     public JTextArea getTextAreaForIndex(int index){
         if(index != -1){
             JPanel panel = (JPanel) this.getTabbedPane().getComponentAt(index);
@@ -119,7 +136,7 @@ public class Gui {
         return null;
     }
 
-
+    //get the text area of the selected focused tab
     public JTextArea getTextAreaForSelectedTab(){
 
         JPanel panel  = (JPanel)this.getTabbedPane().getSelectedComponent();
@@ -127,6 +144,7 @@ public class Gui {
         return (JTextArea)scrollPane.getViewport().getView();
     }
 
+    //get a file that the user has chosen to open through a dialog
     public File chooseFileToOpen(){
         JFileChooser fileChooser = new JFileChooser();
         int res = fileChooser.showOpenDialog(this.getFrame());
@@ -136,53 +154,7 @@ public class Gui {
         return null;
     }
 
-    private void initTabbedPane(){
-
-        this.tabPane  = new JTabbedPane();
-        //addNewTab("untitled", "");
-    }
-
-    public JToolBar getToolBar(){
-        if(this.toolBar == null){
-            initToolBar();
-        }
-        return this.toolBar;
-    }
-
-    private void initToolBar(){
-
-        this.toolBar = new JToolBar();
-        this.toolBar.add(getFileButton());
-        this.toolBar.add(getEditButton());
-
-    }
-
-    public JButton getFileButton(){
-
-        if(this.fileButton == null){
-            initFileButton();
-        }
-        return this.fileButton;
-    }
-
-    private void initFileButton(){
-
-        this.fileButton = new JButton("file");
-
-    }
-
-    public JButton getEditButton(){
-
-        if(this.editButton == null){
-            initEditButton();
-        }
-        return this.editButton;
-    }
-
-    private void initEditButton(){
-        this.editButton = new JButton("edit");
-    }
-
+    //get the menu bar
     public JMenuBar getMenuBar(){
         if(this.menuBar == null){
              initMenuBar();
@@ -190,10 +162,12 @@ public class Gui {
         return this.menuBar;
     }
 
+    //init the open menu item
     private void initOpenItem(){
         this.openItem = new JMenuItem("Open...");
     }
 
+    //get the open menu item
     public JMenuItem getOpenItem(){
         if(this.openItem == null){
             initOpenItem();
@@ -201,10 +175,12 @@ public class Gui {
         return openItem;
     }
 
+    //init the save menu item
     public void initSaveItem(){
         this.saveItem = new JMenuItem("Save");
     }
 
+    //get the save menu item
     public JMenuItem getSaveItem(){
         if(this.saveItem == null){
             initSaveItem();
@@ -212,10 +188,12 @@ public class Gui {
         return this.saveItem;
     }
 
+    //init the save menu item
     private void initSaveAsItem(){
         this.saveAsItem = new JMenuItem("Save As...");
     }
 
+    //get the save as menu item
     public JMenuItem getSaveAsItem(){
         if(this.saveAsItem == null){
             initSaveAsItem();
@@ -223,11 +201,12 @@ public class Gui {
         return this.saveAsItem;
     }
 
+    //init the cipher menu item
     private void initCipherItem(){
         this.cipherItem = new JMenuItem("Cipher...");
     }
 
-
+    //get the cipher menu item
     public JMenuItem getCipherItem(){
         if(this.cipherItem == null){
             initCipherItem();
@@ -235,10 +214,12 @@ public class Gui {
         return this.cipherItem;
     }
 
+    //init the new menu item
     private void initNewItem(){
         this.newItem = new JMenuItem("New...");
     }
 
+    //get the new menu item
     public JMenuItem getNewItem(){
         if(this.newItem == null){
             initNewItem();
@@ -246,6 +227,7 @@ public class Gui {
         return this.newItem;
     }
 
+    //init the menu bar. adds all of our menu items under the appropriate menu
     private void initMenuBar(){
         this.menuBar = new JMenuBar();
         JMenu menu = new JMenu("File");
@@ -257,6 +239,8 @@ public class Gui {
         this.menuBar.add(menu);
     }
 
+    //init all components of the gui.
+    //any new component feature must be called here to be initialized and added to the frame
     private void initComponents(){
         initController();
         JFrame frame = this.getFrame();
