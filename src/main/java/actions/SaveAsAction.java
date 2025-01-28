4d4712d0fabcb41d31df21323b6eda79afadf444
zach <ke3n1@proton.me>
2025-01-27 21:26:07 -0500

    still has a bug when using the ctrl+shit+s shortcut on an untitled tab -- it saves the tab to disk with the new file name but the ctrl+s shortcut DOES NOT WORK after the new title is updated on the tab(after the original ctrl+shift+s operation)

--- src/main/java/actions/SaveAsAction.java
+++ src/main/java/actions/SaveAsAction.java
@@ -26,7 +26,7 @@ public class SaveAsAction extends AbstractMenuAction{
     }
 
     @Override
-    public void actionPerformed(ActionEvent event){
+    public void actionPerformed(ActionEvent event) {
         //user is either trying to save a new file that has no associated file,
         //OR they are trying to create a copy of an existing file
         //the existing file will have a non-null model associated with it based on the title
@@ -40,54 +40,54 @@ public class SaveAsAction extends AbstractMenuAction{
         FileModel model = this.getController().getFileModelForTab(title);
 
         File file = this.getController().getGui().chooseFileToSave();
-        String newTitle = file.getName();
-        //if the model is null this is a completely new file, need to save it and update the tab title
-        try{
-            if(model == null){
-                //init the model and save the content
-                model = new FileModel(file);
-                String update = model.saveContent(content);
-                //update the status label in the view
-                this.getController().updateStatus(update);
-                //add the new model to the model map given the new title
-                this.getController().putFileModelForTab(newTitle, model);
-                //set the new title in the view
-                this.getController().getGui().setTabTitle(index, newTitle);
+        if (file != null) {
+            String newTitle = file.getName();
+            //if the model is null this is a completely new file, need to save it and update the tab title
+            try {
+                if (model == null) {
+                    //init the model
+                    model = new FileModel(file);
+                    //add the new model to the model map given the new title
+                    this.getController().putFileModelForTab(newTitle, model);
+
+                    String update = model.saveContent(content);
+                    //update the status label in the view
+                    this.getController().updateStatus(update);
+
+                    //set the new title in the view
+                    this.getController().getGui().setTabTitle(index, newTitle);
 
 
+                    //if the model is NOT null, this is a COPY, a new model needs to be saved and placed given teh selected file's name
+                    //THEN we need to create a NEW with the copy's title and content.
+                    //the original model should also be saved
+                } else {
+
+                    //create the new model
+                    FileModel newModel = new FileModel(file);
+                    this.getController().putFileModelForTab(newTitle, newModel);
+
+                    //update the new model with the content
+                    String update = newModel.saveContent(content);
+                    //send the status update
+                    this.getController().updateStatus(update);
+                    //set the new model associated with the new title in the map
+                    //add the new(copy) to the view
+                    this.getController().addNewTabToView(newTitle, content);
 
 
-                //if the model is NOT null, this is a COPY, a new model needs to be saved and placed given teh selected file's name
-                //THEN we need to create a NEW with the copy's title and content.
-                //the original model should also be saved
-            }else{
-
-                //create the new model
-                FileModel newModel = new FileModel(file);
-                //update the new model with the content
-                String update = model.saveContent(content);
-                //send the status update
-                this.getController().updateStatus(update);
-                //set the new model associated with the new title in the map
-                this.getController().putFileModelForTab(newTitle, newModel);
-                //add the new(copy) to the view
-                this.getController().addNewTabToView(newTitle, content);
+                    //we also know that the current model exists -- this should be updated as well?
+                    model.saveContent(content);
+                    //if this is what we want to do we SHOULD NOT update the status label here as that might
+                    //be confusing for users
 
 
-                //we also know that the current model exists -- this should be updated as well?
-                model.saveContent(content);
-                //if this is what we want to do we SHOULD NOT update the status label here as that might
-                //be confusing for users
-
-
-
+                }
+            } catch (IOException e) {
+                System.out.printf("Error saving content: %s", e.getMessage());
 
             }
-        }catch(IOException e){
-            System.out.printf("Error saving content: %s", e.getMessage());
-
         }
-
-
     }
+
 }
