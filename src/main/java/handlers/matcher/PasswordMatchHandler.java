50eec0c716ec9a4793a4fb081560c61eecf5b21a
zach <ke3n1@proton.me>
2025-02-03 20:40:28 -0500

    comments

--- src/main/java/handlers/matcher/PasswordMatchHandler.java
+++ src/main/java/handlers/matcher/PasswordMatchHandler.java
@@ -6,17 +6,27 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.util.Arrays;
+/*
+* password match handler - handles the matching of passwords on change of content in the password field and the confirm
+* field of the keystore setup panel
+*/
 
 public class PasswordMatchHandler implements DocumentListener {
+
+    //requires the controller to operate on the required fields
     private final KeyStoreSetupController controller;
+
+    //constructor
     public PasswordMatchHandler(KeyStoreSetupController controller){
         this.controller = controller;
     }
 
+    //getter for controller
     public KeyStoreSetupController getController(){
         return this.controller;
     }
 
+    //override the insert, remove, and changed update methods
     @Override
     public void insertUpdate(DocumentEvent e){
         checkPasswords();
@@ -32,7 +42,7 @@ public class PasswordMatchHandler implements DocumentListener {
         checkPasswords();
     }
 
-
+    //check passwords function that retieves the current entries in the pw and confirm fields to ensure they match
     private void checkPasswords(){
         char[] pw = this.getController().getSetupPanel().getPasswordField().getPassword();
         char[] confirmPw = this.getController().getSetupPanel().getConfirmField().getPassword();
--- src/main/java/handlers/matcher/PasswordReqMatchHandler.java
+++ src/main/java/handlers/matcher/PasswordReqMatchHandler.java
@@ -1,19 +1,31 @@
 package handlers.matcher;
 
 import controllers.KeyStoreSetupController;
-
-import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import java.util.Arrays;
+
+/*
+* password requirement match handler - checks the password to ensure it meets the defined requirements for the program
+* provides visual feedback via foreground coloring of requirements
+*/
 
 public class PasswordReqMatchHandler implements DocumentListener {
+
+    //requires the controller for the keystore setup panel
     KeyStoreSetupController controller;
 
+
+    //constructor
     public PasswordReqMatchHandler(KeyStoreSetupController controller){
         this.controller = controller;
     }
 
+    //getter for controller
+    public KeyStoreSetupController getController(){
+        return this.controller;
+    }
+
+    //overrides for the insert, remove, and change methods
     @Override
     public void insertUpdate(DocumentEvent e) {
         checkRequirements();
@@ -29,10 +41,8 @@ public class PasswordReqMatchHandler implements DocumentListener {
         checkRequirements();
     }
 
-    public KeyStoreSetupController getController(){
-        return this.controller;
-    }
 
+    //check the password against the requirements provided
     public void checkRequirements(){
 
         char[] pw = this.getController().getSetupPanel().getPasswordField().getPassword();
@@ -41,17 +51,22 @@ public class PasswordReqMatchHandler implements DocumentListener {
             this.getController().getSetupPanel().getPwReqFeedbackLabel().setText("");
             return;
         }
+
         boolean[] requirements = new boolean[5];
+
         requirements[0] = pw.length >= 12;
         requirements[1] = hasUpperCase(pw);
         requirements[2] = hasLowerCase(pw);
         requirements[3] = hasNumber(pw);
         requirements[4] = hasSpecial(pw);
+
         updateRequirementsList(requirements);
         this.getController().setRequirementsMet(allRequirementsMet(requirements));
     }
 
+    //check if all the requirements have been met
     public boolean allRequirementsMet(boolean[] requirements){
+
         for(boolean b : requirements){
             if(!b){
                 return false;
@@ -59,8 +74,12 @@ public class PasswordReqMatchHandler implements DocumentListener {
         }
         return true;
     }
+
+    //update the requirements list visual feedback depending of if the requirement was met or not
     private void updateRequirementsList(boolean[] requirements){
+
         StringBuilder html = new StringBuilder("<html>");
+
         String[] reqDescriptions ={
                 "Minimum 12 characters",
                 "At least one uppercase letter",
@@ -68,45 +87,68 @@ public class PasswordReqMatchHandler implements DocumentListener {
                 "At least one number",
                 "At least one special character"
         };
+
         for(int i = 0; i < requirements.length; i++){
+
             String color = requirements[i] ? "green": "red";
             html.append(String.format("<font color = '%s'>%s</font><br>", color, reqDescriptions[i]));
+
         }
+
         html.append("</html>");
         this.getController().getSetupPanel().getPwReqFeedbackLabel().setText(html.toString());
     }
 
+    //one uppercase character requirement
     private boolean hasUpperCase(char[] pw){
+
         for(char c : pw){
             if(Character.isUpperCase(c)){
                 return true;
             }
         }
+
         return false;
+
     }
+
+    //one lowercase character requirement
     private boolean hasLowerCase(char[] pw){
+
         for(char c : pw){
             if(Character.isLowerCase(c)){
                 return true;
             }
         }
+
         return false;
+
     }
+
+    //one number character requirement
     private boolean hasNumber(char[] pw){
+
         for(char c : pw){
             if(Character.isDigit(c)){
                 return true;
             }
         }
+
         return false;
+
     }
+
+    //one special character requirement
     private boolean hasSpecial(char[] pw){
+
         for(char c : pw){
             if(!Character.isLetterOrDigit(c) && !Character.isWhitespace(c)){
                 return true;
             }
         }
+
         return false;
+
     }
 
 
