e10751a939adbb08791172fb6bcd2649d1a9f3a4
zach <ke3n1@proton.me>
2025-02-22 21:24:12 -0500

    trying to get installer built

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module cryptopad {
+    requires java.desktop;
+    requires com.formdev.flatlaf;
+    requires com.formdev.flatlaf.intellijthemes;
+    requires org.bouncycastle.provider;
+
+    exports actions;
+    exports components;
+    exports controllers;
+    exports drivers;
+    exports handlers.matcher;
+    exports handlers.menu;
+    exports handlers.tabs;
+    exports models;
+    exports util;
+    exports util.enums;
+    exports util.constants;
+}
