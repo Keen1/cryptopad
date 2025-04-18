ba909405eb0daa8548fa6779c8e1cc8d6db77ade
zach <ke3n1@proton.me>
2025-04-18 04:25:25 -0400

    added setters

--- src/main/java/controllers/PreferencesController.java
+++ src/main/java/controllers/PreferencesController.java
@@ -60,11 +60,20 @@ public class PreferencesController {
     public String getTheme(){
         return this.getModel().getTheme();
     }
+    public void setTheme(String theme){
+        this.getModel().setTheme(theme);
+    }
     public String getFontFamily(){
         return this.getModel().getFontFamily();
     }
+    public void setFontFamily(String fontFamily){
+        this.getModel().setFontFamily(fontFamily);
+    }
     public int getFontSize(){
         return this.getModel().getFontSize();
+    }
+    public void setFontSize(int fontSize){
+        this.getModel().setFontSize(fontSize);
     }
 
 
