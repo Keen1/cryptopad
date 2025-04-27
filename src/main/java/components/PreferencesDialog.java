2115ae8c6ebcc80bd3a9cd0e7b874cb985eb7185
zach <ke3n1@proton.me>
2025-04-26 21:41:19 -0400

    comments, reduced accessibility of some functions in preferences dialog to private

--- src/main/java/components/PreferencesDialog.java
+++ src/main/java/components/PreferencesDialog.java
@@ -9,8 +9,13 @@ import java.text.NumberFormat;
 import java.util.HashMap;
 import javax.swing.UIManager.LookAndFeelInfo;
 
+/*
+* Preferences dialog -- allows user to customize the application.
+*/
+
 public class PreferencesDialog extends JDialog {
 
+    //components
     private JComboBox<String> themeComboBox;
     private JComboBox<String> fontFamilyComboBox;
     private JFormattedTextField fontSizeField;
@@ -18,19 +23,23 @@ public class PreferencesDialog extends JDialog {
     private JButton cancelButton;
     private HashMap<String, String> lafMap;
 
+    //preferences controller
     private final PreferencesController controller;
 
+
+    //default constructor
     public PreferencesDialog(PreferencesController controller, JFrame parentFrame, String title, boolean modal){
         super(parentFrame, title, modal);
         this.controller = controller;
         initComponents();
     }
 
+    //getter for controller
     public PreferencesController getController(){
         return this.controller;
     }
 
-    //TODO init all the components and lay out the dialog
+    //initiate the components and pack the dialog
     public void initComponents(){
 
         setLayout(new BorderLayout());
@@ -82,7 +91,7 @@ public class PreferencesDialog extends JDialog {
 
     }
 
-
+    //getter for the cancel button
     public JButton getCancelButton(){
 
         if(this.cancelButton == null){
@@ -92,11 +101,12 @@ public class PreferencesDialog extends JDialog {
         return this.cancelButton;
     }
 
-    public void initCancelButton(){
+    //init function for the cancel button
+    private void initCancelButton(){
         this.cancelButton = new JButton("cancel");
     }
 
-
+    //getter for apply button
     public JButton getApplyButton(){
 
         if(this.applyButton == null){
@@ -106,12 +116,13 @@ public class PreferencesDialog extends JDialog {
         return this.applyButton;
     }
 
-    public void initApplyButton(){
+    //init function for apply button
+    private void initApplyButton(){
         this.applyButton = new JButton("apply");
     }
 
 
-
+    //getter for the font size field
     public JFormattedTextField getFontSizeField(){
 
         if(this.fontSizeField == null){
@@ -121,12 +132,15 @@ public class PreferencesDialog extends JDialog {
         return this.fontSizeField;
     }
 
-    public void initFontSizeField(){
+    //init function for font size field
+    //TODO -- need to test and make sure these formatters work currently it just lets you type in whatever doesn't restrict
+    private void initFontSizeField(){
         this.fontSizeField = new JFormattedTextField(NumberFormat.getIntegerInstance());
         this.fontSizeField.setValue(this.getController().getFontSize());
 
     }
 
+    //getter for the font family combo box
     public JComboBox<String> getFontFamilyComboBox(){
 
         if(this.fontFamilyComboBox == null){
@@ -136,7 +150,8 @@ public class PreferencesDialog extends JDialog {
         return this.fontFamilyComboBox;
     }
 
-    public void initFontFamilyComboBox(){
+    //init function for the font family combo box
+    private void initFontFamilyComboBox(){
 
         this.fontFamilyComboBox = new JComboBox<>(getFontNames());
         String currentFont = this.getController().getFontFamily();
@@ -144,11 +159,12 @@ public class PreferencesDialog extends JDialog {
 
     }
 
+    //getter for local graphics env fonts available
     public String[] getFontNames(){
         return GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
     }
 
-
+    //getter for the theme combo box
     public JComboBox<String> getThemeComboBox(){
 
         if(this.themeComboBox == null){
@@ -158,27 +174,33 @@ public class PreferencesDialog extends JDialog {
         return this.themeComboBox;
     }
 
-    public void initThemeComboBox(){
+    //init function for the theme combo box
+    private void initThemeComboBox(){
         this.themeComboBox = new JComboBox<>(getLafNames());
 
     }
 
-    public String[] getLafNames(){
+    //utility function to retrieve look and feel names
+    private String[] getLafNames(){
         return this.getLafMap().keySet().toArray(new String[0]);
     }
 
+    //getter for the selected item of the theme combo box
     public String getSelectedTheme(){
         return (String)this.getThemeComboBox().getSelectedItem();
     }
 
+    //getter for the selected item of the font family combo box
     public String getSelectedFontFamily(){
         return (String)this.getFontFamilyComboBox().getSelectedItem();
     }
 
+    //get the inputted font size from the font size field
     public int getSelectedFontSize(){
         return Integer.parseInt(this.getFontSizeField().getText());
     }
 
+    //getter for  the look and feel map
     public HashMap<String, String> getLafMap(){
 
         if(this.lafMap == null){
@@ -187,10 +209,10 @@ public class PreferencesDialog extends JDialog {
 
         return this.lafMap;
     }
-
-    public void initLafMap(){
+    //init function for the look and feel map
+    private void initLafMap(){
         this.lafMap = new HashMap<>();
-
+        //TODO -- check if there is a way to not hardcode this.
         this.lafMap.put("FlatLaf Light", "com.formdev.flatlaf.FlatLightLaf");
         this.lafMap.put("FlatLaf Dark", "com.formdev.flatlaf.FlatDarkLaf");
         this.lafMap.put("FlatLaf Intellij", "com.formdev.flatlaf.FlatIntellijLaf");
@@ -207,7 +229,4 @@ public class PreferencesDialog extends JDialog {
         this.getController().setLafMap(this.lafMap);
 
     }
-
-
-
 }
