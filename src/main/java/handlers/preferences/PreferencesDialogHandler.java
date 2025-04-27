1697db1552a9088c5773cd948f4970a4d1d62067
zach <ke3n1@proton.me>
2025-04-26 21:45:32 -0400

    commented out the preferences dialog handler

--- src/main/java/handlers/preferences/PreferencesDialogHandler.java
+++ src/main/java/handlers/preferences/PreferencesDialogHandler.java
@@ -4,14 +4,21 @@ import controllers.PreferencesController;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+/*
+* Preferences Dialog Handler -- handles user applying preferences in the preferences dialog
+*/
 public class PreferencesDialogHandler implements ActionListener {
+
+    //controller
     private final PreferencesController controller;
 
+    //constructor
     public PreferencesDialogHandler(PreferencesController controller){
         this.controller = controller;
     }
 
+    //override of action performed, pulls preferences from the dialog then saves them, then sets them
+    //TODO -- this might need access to the frame if we want to update the component tree from here.
     @Override
     public void actionPerformed(ActionEvent event){
 
@@ -23,6 +30,7 @@ public class PreferencesDialogHandler implements ActionListener {
 
     }
 
+    //getter for the dialog controller
     public PreferencesController getController(){
         return this.controller;
     }
