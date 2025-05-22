4e541fabea23f36e67fe4c94d47aded9d411dc43
zach <ke3n1@proton.me>
2025-05-21 23:41:22 -0400

    starting tests on cipher dialog

--- src/test/java/components/CipherDialogTest.java
+++ src/test/java/components/CipherDialogTest.java
@@ -1,4 +1,33 @@
 package components;
 
+
+import components.dialogs.CipherDialog;
+import components.panels.MainPanel;
+import controllers.KeyStoreController;
+import controllers.MainPanelController;
+import models.KeyStoreModel;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
 public class CipherDialogTest {
+    private static CipherDialog dialog;
+
+    @BeforeAll
+    public static void setup(){
+
+        KeyStoreController keyStoreController = new KeyStoreController();
+        KeyStoreModel model = new KeyStoreModel();
+        MainPanel panel = new MainPanel(model);
+        MainPanelController mainPanelController = new MainPanelController(panel);
+        dialog = new CipherDialog(keyStoreController, mainPanelController);
+
+    }
+
+    //test that generating a key generates a non-null value for the generated key attribute of the dialog
+    @Test
+    public void generatedKeyTest(){
+
+    }
 }
